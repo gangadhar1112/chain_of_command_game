2b693d4ebfe7d12f8cfedc9f9bed6427ebd01052
Gangadhar <gangadharg1112@gmail.com>
2025-05-29 23:24:57 +0530

    Configure Firebase and Start Dev Server

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  databaseURL: "YOUR_DATABASE_URL"
+  apiKey: "AIzaSyBOvBtmHZI_mOX_9_5WxqLBkZR3BXJIh8Q",
+  authDomain: "chain-of-command-game.firebaseapp.com",
+  databaseURL: "https://chain-of-command-game-default-rtdb.firebaseio.com",
+  projectId: "chain-of-command-game",
+  storageBucket: "chain-of-command-game.appspot.com",
+  messagingSenderId: "123456789012",
+  appId: "1:123456789012:web:abc123def456ghi789jkl"
 };
 
 const app = initializeApp(firebaseConfig);
